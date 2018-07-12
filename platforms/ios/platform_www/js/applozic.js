1facce7694e1c81f7a8460c7af83a294ce25e355
MuraliYarra <murali.uideveloper@gmail.com>
2018-07-12 12:57:26 +0530

    Initial commit

---
+++ platforms/ios/platform_www/js/applozic.js
@@ -0,0 +1,58 @@
+/*global cordova, module*/
+
+module.exports = {
+    login: function(alUser, successCallback, errorCallback) {
+    	cordova.exec(successCallback, errorCallback, "ApplozicCordovaPlugin", "login", [JSON.stringify(alUser)]);
+    },
+    registerPushNotification: function(successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "ApplozicCordovaPlugin", "registerPushNotification", []);
+    },
+    isLoggedIn: function(successCallback, errorCallback) {
+    	cordova.exec(successCallback, errorCallback, "ApplozicCordovaPlugin", "isLoggedIn", []);
+    },
+    getUnreadCount: function(successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "ApplozicCordovaPlugin", "getUnreadCount", []);
+    },
+    updatePushNotificationToken: function(token, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "ApplozicCordovaPlugin", "updatePushNotificationToken", [token]);
+    },
+    launchChat: function(successCallback, errorCallback) {
+    	cordova.exec(successCallback, errorCallback, "ApplozicCordovaPlugin", "launchChat", []);
+    },
+    launchChatWithUserId: function(userId, successCallback, errorCallback) {
+    	cordova.exec(successCallback, errorCallback, "ApplozicCordovaPlugin", "launchChatWithUserId", [userId]);
+    },
+    launchChatWithGroupId: function(groupId, successCallback, errorCallback) {
+    	cordova.exec(successCallback, errorCallback, "ApplozicCordovaPlugin", "launchChatWithGroupId", [groupId]);
+    },
+    launchChatWithClientGroupId: function(clientGroupid, successCallback, errorCallback) {
+    	cordova.exec(successCallback, errorCallback, "ApplozicCordovaPlugin", "launchChatWithClientGroupId", [clientGroupid]);
+    },
+    startNewConversation: function(selected, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "ApplozicCordovaPlugin", "startNewConversation", [selected]);
+    },
+    showAllRegisteredUsers: function(showAllContacts, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "ApplozicCordovaPlugin", "showAllRegisteredUsers", [showAllContacts]);
+    },
+    addContact: function(contact, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "ApplozicCordovaPlugin", "addContact", [JSON.stringify(contact)]);
+    },
+    updateContact: function(contact, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "ApplozicCordovaPlugin", "updateContact", [JSON.stringify(contact)]);
+    },
+    removeContact: function(contact, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "ApplozicCordovaPlugin", "removeContact", [JSON.stringify(contact)]);
+    },
+    addContacts: function(contacts, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "ApplozicCordovaPlugin", "addContacts", [JSON.stringify(contacts)]);
+    },
+    processPushNotification: function(data, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "ApplozicCordovaPlugin", "processPushNotification", [JSON.stringify(data)]);
+    },
+    createGroup: function(group, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "ApplozicCordovaPlugin", "createGroup", [JSON.stringify(group)]);
+    },
+    logout: function(successCallback, errorCallback) {
+    	cordova.exec(successCallback, errorCallback, "ApplozicCordovaPlugin", "logout", []);
+    }
+};
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+
+declare var applozic;
 
 @Component({
   selector: 'page-home',
@@ -9,6 +11,67 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad() {
+    this.loginApplozic({
+      firstName: 'David',
+      lastName: 'Allen',
+      id: '0814fa90-1f1b-49c8-89e8-38d6ba00608f'
+    });
+  }
+
+  loginApplozic(patient) {
+
+    let contacts = [
+      {'userId': patient.id, 'displayName': patient.firstName + " " + patient.lastName}
+    ];
+
+    applozic.addContacts(contacts, function () {
+      const alUser = {
+        'userId': '0814fa90-1f1b-49c8-89e8-38d6ba00608f',   //Replace it with the userId of the logged in user NOTE : userId need to be string and  +,*,? are not allowed chars in userId.
+        // 'password': 'password',  //Put password here
+        'authenticationTypeId': 1,
+        'applicationId': 'caremon21b4cd536d4ccfd45f1209b',  //replace "applozic-sample-app" with Application Key from Applozic Dashboard
+        'displayName': patient.firstName + " " + patient.lastName //Set   the display name of user to show in chat screen
+      };
+      applozic.login(alUser, function () {
+        // alert(alUser.userId + "loggied in successfully")
+        applozic.registerPushNotification(function (res) {
+          console.log("registered successfully" + res);
+          // alert("registered successfully" + res);
+        }, function (err) {
+          console.log("registered errors" + err);
+        });
+
+        //  applozic.launchChatWithUserId('7fa8499f-5480-40b6-88cf-c137f1f85d02', function() {
+        //   // applozic.launchChatWithUserId('0814fa90-1f1b-49c8-89e8-38d6ba00608', function() {
+        //   console.log("success");
+        // }, function () {
+        //   console.log("error");
+        // });
+        //  applozic.showAllRegisteredUsers(true, function(res) {
+        //   alert(res);
+        // }, function() {});
+      }, function (err) {
+        console.log("login errors" + err);
+      });
+    }, function () {
+
+    });
+  }
+
+  launchChat() {
+    applozic.launchChat(function (res) {
+      console.log("chat" + res);
+      alert("chat" + res);
+
+      applozic.showAllRegisteredUsers(true, function () {
+      }, function () {
+      });
+    }, function (err) {
+      console.log("chat errors" + err);
+    });
   }
 
 }
